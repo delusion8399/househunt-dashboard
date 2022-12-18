4d666df2c8ea4d99ae1e925c11fb71e9ba6c6e24
delusion8399 <delusion8399@gmail.com>
2022-12-18 22:48:44 +0530

    cleanup code

--- src/utils/dateUtils.ts
+++
@@ -1,5 +0,0 @@
-import { format, parse } from 'date-fns';
-
-export function formatSplynxDate(dateString: string, formatString: string) {
-  return format(parse(dateString, 'yyyy-MM-dd', new Date()), formatString);
-}
--- src/utils/objectUtils.ts
+++
@@ -1,9 +0,0 @@
-export function omitIf(obj: any, condition: any) {
-  const newObj = { ...obj };
-  Object.keys(newObj).forEach((k) => {
-    if (obj[k] === condition) {
-      delete obj[k];
-    }
-  });
-  return newObj;
-}
